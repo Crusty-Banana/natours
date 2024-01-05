be32663018509af32e267dd5c97d428013c45983
CrustyBanana <dodoankhac@gmail.com>
2024-01-05 18:53:29 +0700

    feat: add field limiting  to tour get request

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -25,6 +25,14 @@ exports.getAllTours = async (req, res) => {
             query = query.sort('-createdAt');
         }
 
+        // 3) Field Limiting
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        } else {
+            query = query.sort('-__V');
+        }
+
         // EXECUTE QUERY
         const tours = await query;
         res.status(200).json({
