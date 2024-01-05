492e59d9d77bb522c4858a39761e245ef1edbfd3
CrustyBanana <dodoankhac@gmail.com>
2024-01-05 21:26:53 +0700

    feat: add tour-stats

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -105,3 +105,41 @@ exports.deleteTour = async (req, res) => {
         });
     }
 };
+
+exports.getTourStats = async (req, res) => {
+    try {
+        const stats = await Tour.aggregate([
+            {
+                $match: { ratingsAverage: { $gte: 4.5 } },
+            },
+            {
+                $group: {
+                    _id: { $toUpper: '$difficulty' },
+                    num: { $sum: 1 },
+                    numRatings: { $sum: '$ratingsQuantity' },
+                    avgRating: { $avg: '$ratingsAverage' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' },
+                },
+            },
+            {
+                $sort: { avgPrice: 1 },
+            },
+            // {
+            //     $match: { _id: { $ne: 'EASY' } },
+            // },
+        ]);
+        res.status(201).json({
+            status: 'success',
+            data: {
+                stats: stats,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failed',
+            message: 'No tour found with such id',
+        });
+    }
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,6 +9,7 @@ router
     .route('/top-5-cheap')
     .get(tourController.aliasTop5Cheap, tourController.getAllTours);
 
+router.route('/tour-stats').get(tourController.getTourStats);
 router
     .route('/')
     .get(tourController.getAllTours)
