e5052fdcaa75f4b219beb203ef115e51ca889356
CrustyBanana <dodoankhac@gmail.com>
2024-01-05 18:34:56 +0700

    feat: add advanced filtering to tour post request

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -2,10 +2,20 @@ const Tour = require('../models/tours');
 
 exports.getAllTours = async (req, res) => {
     try {
-        const queryObj = { ...req.query };
+        // BUILD QUERY
+        // 1) Filtering
+        let queryObj = { ...req.query };
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
         excludedFields.forEach((el) => delete queryObj[el]);
 
+        // 2) Advanced Filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(
+            /\b(gte|gt|lte|lt)\b/g,
+            (match) => `$${match}`,
+        );
+        console.log(JSON.parse(queryStr));
+        queryObj = JSON.parse(queryStr);
         const query = Tour.find(queryObj);
         const tours = await query;
         res.status(200).json({
@@ -18,7 +28,7 @@ exports.getAllTours = async (req, res) => {
     } catch (err) {
         res.status(400).json({
             status: 'failure',
-            message: 'something went wrong',
+            message: err,
         });
     }
 };
